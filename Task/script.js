281dd3feaec566234e097462ea236aba25ec70b0
Shivam <shivamch0865@gmail.com>
2026-03-25 13:58:18 +0530

    Html and css completed

---
+++ Task/script.js
@@ -0,0 +1,11 @@
+let cardContainer = document.getElementById('cards-container');
+let hpCard = document.getElementById('hpCard');
+let acerCard = document.getElementById('acerCard');
+let lenovoCard = document.getElementById('lenovoCard');
+let cartBtn = document.getElementById('cart-btn');
+let buyBtn = document.getElementById('buy-btn');
+
+
+
+let cartPannel = document.createElement('div')
+
